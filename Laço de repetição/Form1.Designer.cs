c05164e8936834cc5e5d22c34133b2eb744a1f14
João Pedro <112526011+JohnPdro@users.noreply.github.com>
2023-03-01 19:35:41 -0300

    Adicionar arquivos de projeto.

---
+++ Laço de repetição/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿
+namespace Laço_de_repetição
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstFor = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstFor
+            // 
+            this.lstFor.BackColor = System.Drawing.Color.Cornsilk;
+            this.lstFor.Font = new System.Drawing.Font("OCR A Extended", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstFor.FormattingEnabled = true;
+            this.lstFor.ItemHeight = 12;
+            this.lstFor.Location = new System.Drawing.Point(23, 12);
+            this.lstFor.Name = "lstFor";
+            this.lstFor.Size = new System.Drawing.Size(150, 256);
+            this.lstFor.TabIndex = 0;
+            this.lstFor.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Wheat;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("OCR-A BT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(179, 229);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 47);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Clique aqui";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SlateBlue;
+            this.ClientSize = new System.Drawing.Size(290, 295);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lstFor);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstFor;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Laço de repetição/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laço_de_repetição
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //lstFor.Items.Add(1);
+            //lstFor.Items.Add(2);
+            //lstFor.Items.Add(3);
+            //lstFor.Items.Add(4);
+            //lstFor.Items.Add(5);
+            //lstFor.Items.Add(6);
+            //lstFor.Items.Add(7);
+            //lstFor.Items.Add(8);
+            //lstFor.Items.Add(9);
+            //lstFor.Items.Add(10);
+        }
+    }
+}
